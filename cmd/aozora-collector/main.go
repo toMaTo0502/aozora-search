d101f6a1873fcac3bfe7514248b4f14bfcc05a03
kawauchi toma <juju4415@icloud.com>
2023-06-02 00:36:22 +0900

    テストケース実装

--- cmd/aozora-collector/main.go
+++ cmd/aozora-collector/main.go
@@ -30,6 +30,8 @@ type Entry struct {
 	ZipURL   string
 }
 
+var pageURLFormat = "https://www.aozora.gr.jp/cards/%s/card%s.html"
+
 func findEntries(siteURL string) ([]Entry, error) {
 	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
 	entries := []Entry{}
@@ -44,7 +46,7 @@ func findEntries(siteURL string) ([]Entry, error) {
 			return
 		}
 		title := elem.Text()
-		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
+		pageURL := fmt.Sprintf(pageURLFormat, token[1], token[2])
 		author, zipURL := findAuthorAndZIP(pageURL)
 		if zipURL != "" {
 			entries = append(entries, Entry{
